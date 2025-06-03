6649dcca5cd2c0bd701de095f8f129a8ddd9721f
Andrés Felipe Mariño Villamil <andres_fmarino@soy.sena.edu.co>
2025-06-02 20:01:51 -0500

    Update LibroTestController.java

--- src/main/java/edu/sena/bibliotecaspring/controller/LibroTestController.java
+++ src/main/java/edu/sena/bibliotecaspring/controller/LibroTestController.java
@@ -1,24 +1,31 @@
 package edu.sena.bibliotecaspring.controller;
 
 import edu.sena.bibliotecaspring.model.Libro;
+import edu.sena.bibliotecaspring.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import edu.sena.bibliotecaspring.service.LibroService;
+
+
 @RestController
 @RequestMapping("/api/libros")
 public class LibroTestController {
-    @Autowired
-    private LibroService libroService; // Asegúrate de tener un servicio para manejar la lógica de negocio
+
+    private final LibroService libroService;
+
+    public LibroTestController(LibroService libroService) {
+        this.libroService = libroService;
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Libro> getLibroById(@PathVariable Long id) {
+    public ResponseEntity<Libro> getLibrosById(@PathVariable Long id) {
         Libro libro = libroService.findById(id);
         if (libro == null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(libro);
     }
+
     @PostMapping
     public ResponseEntity<Libro> crearLibro(@RequestBody Libro libro) {
         Libro nuevoLibro = libroService.save(libro);
@@ -27,11 +34,11 @@ public class LibroTestController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Libro> actualizarLibro(@PathVariable Long id, @RequestBody Libro libro) {
-        Libro libroExistente = libroService.findById(id);
-        if (libroExistente == null) {
+        Libro LibroExistente = libroService.findById(id);
+        if (LibroExistente == null) {
             return ResponseEntity.notFound().build();
         }
-        libro.setId(id); // Aseguramos que el ID se mantenga
+        libro.setId(id);
         Libro libroActualizado = libroService.save(libro);
         return ResponseEntity.ok(libroActualizado);
     }
